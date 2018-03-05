a1255e32a8037f2928e6afb37759396520f7c985
Rober19 <texas_telecaster@hotmail.com>
2018-03-04 19:11:42 -0500

    fix publications - files

--- middlewares/pub_exist.js
+++ middlewares/pub_exist.js
@@ -13,9 +13,10 @@ const dbPublication = require('../model/publication');
 
 exports.find_pub = async function (req, res, next) {
 
-  return res.status(300).send(config.resJson('Parada', 300));
+  
 
   dbPublication.findOne({ _id: req.params.id, user: req.user.sub }, (err, data) => {
+    return res.status(300).send(config.resJson('Parada', 300));
     if (err) return res.status(500).send(config.resJson(config.resMsg.publicationNotFound, 500));
     if (data == null || data == undefined) return res.status(400).send(config.resJson(config.resMsg.publicationNotFound, 400))
     if (data.file && data.file != null) return res.status(400).send(config.resJson(config.resMsg.public_fileExist, 400));
