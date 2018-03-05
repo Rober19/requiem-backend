30457a3fe85317905c224408ed9f19e5492f2bfc
Rober19 <texas_telecaster@hotmail.com>
2018-03-04 23:38:08 -0500

    fix publications - files

--- controller/publicationController.js
+++ controller/publicationController.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const config = require('../config/config');
 const moment = require('moment');
 const mongoosePaginate = require('mongoose-pagination');
-
+const fetch = require('node-fetch');
 const dbPublication = require('../model/publication');
 const dbFollow = require('../model/follow');
 
