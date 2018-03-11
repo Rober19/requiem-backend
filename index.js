5c76a6f525cafbd1d8748d485baf662dde8f3998
Rober19 <texas_telecaster@hotmail.com>
2018-03-11 13:02:28 -0500

    fixes and test

--- index.js
+++ index.js
@@ -5,9 +5,25 @@ const config = require('./config/config');
 const app = require('./app');
 //importar la conexion a mongoose pre-configurada
 const mongoose = require('./config/db-mongoose');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+io.on('connection', socket => {
+
+  socket.on('myNotification', data => {
+
+    if (data.option == 'like') {
+      io.emit('myNotification', data)
+      console.log(data)
+    }
+
+
+  });
+
+});
 
 //abri un server
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   console.log(config.resJson((config.resMsg.serverOn + ' on ' + config.port), 200))
 });
 
