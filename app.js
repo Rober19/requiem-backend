f7272a2e69cfccc767ac8701bb687a169e37a6b3
Rober19 <texas_telecaster@hotmail.com>
2018-03-12 18:54:30 -0500

    fix headers cors

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const messageRouter = require('./routes/message');
 //cors
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, user");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, user, admin_secret");
   next();
 });
 
