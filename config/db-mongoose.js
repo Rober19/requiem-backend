3d8d970bcd09442b56c2594322a58e3ed79c4bd1
unknown <texas_telecaster@hotmail.com>
2018-08-03 21:17:02 -0500

    improv logs and msg

--- config/db-mongoose.js
+++ config/db-mongoose.js
@@ -3,16 +3,17 @@
 const config = require('./config')
 //requerir mongoose para mongoDB
 const mongoose = require('mongoose');
-
+const { green, yellow, cyan, red } = require('chalk');
 //hacer un metodo de promesa para hacer conexión
 mongoose.Promise = global.Promise;
 // conectar con la url de la data base
-mongoose.connect(`mongodb://${config.dev_crendentials.mlab_db}/db-social-mean5`).then( () => {
-  //si se conecta correctamente
-  console.log(config.resJson(config.resMsg.conectionOk, 200))
-}).catch( (err) => {
-  //pero si ocurre un error
-  console.log(config.resJson(config.resMsg.conectionErr, 400))
-});
+mongoose.connect(`mongodb://${config.dev_crendentials.mlab_db}/db-social-mean5`).then(() => {
+  //si se conecta correctamente 
+  console.log(`${cyan(`[${require('../package.json').name}]`)} ${green(`[${config.resMsg.conectionOk}]`)} [Status:${green(` ${200}`)}]`)
+},
+  (err) => {
+    //pero si ocurre un error
+    console.log(`${cyan(`[${require('../package.json').name}]`)} ${red(`[${config.resMsg.conectionErr}]`)} [Status:${red(` ${400}`)}]`)
+  })
 
 module.exports = mongoose;
