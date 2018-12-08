d61449f8209025866b40afc4739f0f32e27cf39b
Rober19 <texas_telecaster@hotmail.com>
2018-12-08 11:12:37 -0500

    [Rober19] [ 12_08_2018] 4

--- deploy.bat
+++ deploy.bat
@@ -11,16 +11,15 @@ RD /s /q tempF1
 echo.
 set /p commit=Enter commit name: 
 git add -A
-for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
-     set dow=%%i
+for /f "tokens=1-3 delims=/ " %%j in ("%date%") do (
+    REM  set dow=%%i
      set month=%%j
      set day=%%k
      set year=%%l
 )
 set p1=%git config user.name%
 echo %p1%
-set datestr=%month%_%day%_%year%
-
+set datestr= %month%_%day%_%year%
 xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\config" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github\config" /e /s /k /q /c /i /y /h
 xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\controller" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github\controller" /e /s /k /q /c /i /y /h
 REM xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\functions" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github\functions" /e /s /k /q /c /i /y /h
@@ -32,6 +31,7 @@ xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\model" "D:\Software\
 xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\package.json" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github" /y
 xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\index.js" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github" /y
 xcopy "D:\Software\ProyectosWEB\SOA\Requiem-\backend-heroku\app.js" "D:\Software\ProyectosWEB\SOA\Requiem-\backend-github" /y
-
+pause>nul
+REM tambien en la fecha se puede usar el %date%
 git commit -m "[%gituser%] [%datestr%] %commit% "
 git push origin master
