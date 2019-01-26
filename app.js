80f8b43e2cb007f2fb5191ff73f9f5aed1dc2149
rober19 <texas_telecaster@hotmail.com>
2019-01-26 16:42:14 -0500

    1.0.40

--- app.js
+++ app.js
@@ -13,27 +13,20 @@ const [userRouter, publicationRouter, followRouter, messageRouter] = [
 
 //cors
 app.use(cors());
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, user, admin_secret");
-//   next();
-// });
 
-//middlewares -: es un metodo que se ejecuta antes que llegue a un controlador
-//usar body-parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(morgan('dev'));
 
 //rutas
-// app.use('/app', followRouter);
-// app.use('/app', messageRouter);
+app.use('/app', followRouter);
+app.use('/app', messageRouter);
 app.use('/app', userRouter);
-// app.use('/app', publicationRouter);
+app.use('/app', publicationRouter);
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Ya!' });
+  res.send({ message: 'Requiem - by Rober19' });
 });
 
 app.use((err, req, res, next) => {
